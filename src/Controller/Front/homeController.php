ce9de8cbba3403d85fa14f68d0da00c2933b5632
omar_aguil <95982059+omaraguil@users.noreply.github.com>
2025-03-28 02:23:01 +0100

    base commit

---
+++ src/Controller/Front/homeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class homeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('Front/home.html.twig');
+    }
+}
